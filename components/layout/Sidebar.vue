7b43b47c62cdd97164297961cddd400be7a308eb
danhalf <danhel@rocketmail.com>
2023-12-25 22:06:35 +0200

    added: settings

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,14 +1,16 @@
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full w-full relative">
     <NuxtLink to="/" class="mb-10 block">
-      <NuxtImg src="/logo.png" class="mx-auto" width="100px"/>
+      <NuxtImg src="/logo.png" class="mx-auto" width="100px" />
     </NuxtLink>
 
-    <UIButton class="absolute text-2xl rounded-full top-2 right-3 transition-colors hover:text-secondary">
-      <Icon name="line-md:logout"/>
+    <UIButton
+      class="absolute text-2xl rounded-full top-2 right-3 transition-colors hover:text-secondary"
+    >
+      <Icon name="line-md:logout" />
+      for commmit
     </UIButton>
   </aside>
 </template>
